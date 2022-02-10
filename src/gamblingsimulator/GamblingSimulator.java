aa8ed547eea0db57ddc18a77ea8b58d173720e86
Priyanka <priyankadombale27@gmail.com>
2022-02-10 17:08:41 +0530

    Added : README.md file

--- src/gamblingsimulator/GamblingSimulator.java
+++ src/gamblingsimulator/GamblingSimulator.java
@@ -1,6 +1,9 @@
 package gamblingsimulator;
 
 public class GamblingSimulator {
+	// Defined variables for stake and bet
+	static int stake = 100;
+	static int betForGame = 1;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome in Gambling Simulator...!!");
