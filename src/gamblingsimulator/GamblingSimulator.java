cd242e5438b224f76663bab318351865e0b7fe8d
Priyanka <priyankadombale27@gmail.com>
2022-02-17 16:53:53 +0530

    Added : Welcome Message

--- src/gamblingsimulator/GamblingSimulator.java
+++ src/gamblingsimulator/GamblingSimulator.java
@@ -1,9 +1,6 @@
 package gamblingsimulator;
 
 public class GamblingSimulator {
-	// Defined variables for stake and bet
-	static int stake = 100;
-	static int betForGame = 1;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome in Gambling Simulator...!!");
