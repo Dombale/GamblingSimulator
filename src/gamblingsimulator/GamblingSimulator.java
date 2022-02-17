1173aa12f7a19a1da8916511340b657ac7536984
Priyanka <priyankadombale27@gmail.com>
2022-02-17 17:17:58 +0530

    Added : Check win Or lose gambler and increment or decrement vastake accrdigly

--- src/gamblingsimulator/GamblingSimulator.java
+++ src/gamblingsimulator/GamblingSimulator.java
@@ -6,7 +6,25 @@ public class GamblingSimulator {
 	static int betForGame = 1;
 
 	public static void main(String[] args) {
+
 		System.out.println("Welcome in Gambling Simulator...!!");
+		winloose(); // call to winloose method
+
+	}
+
+	static void winloose() {
+		int randomNum = (int) (Math.random() * 2); // create random number using math function
+		System.out.print(randomNum);
+
+		if (randomNum == 1) // condition for win
+		{
+			stake++; // stake will increase by 1 if win
+			System.out.println(" You win , stake is : " + stake);
+
+		} else {
+			stake--; // else stake will decrease by 1 if lose
+			System.out.println(" You lose ,stake is :  " + stake);
+		}
 
 	}
 
