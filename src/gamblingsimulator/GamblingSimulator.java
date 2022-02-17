33970cb9a6a31a57fc628ec9bfbd6a00db45de46
Priyanka <priyankadombale27@gmail.com>
2022-02-17 16:57:40 +0530

    Added : Initiaze Stake And Bet

--- src/gamblingsimulator/GamblingSimulator.java
+++ src/gamblingsimulator/GamblingSimulator.java
@@ -1,6 +1,9 @@
 package gamblingsimulator;
 
 public class GamblingSimulator {
+	// Initialization
+	static int stake = 100;
+	static int betForGame = 1;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome in Gambling Simulator...!!");
